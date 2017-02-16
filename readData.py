a69db73eec89d72fa6c3bd52def29287fcdc3823
Bas Vogelzang <bas.vogelzang95@gmail.com>
2017-02-16 17:18:18 +0100

    cleaning the reviews and appending to a list

--- readData.py
+++ readData.py
@@ -1,16 +1,36 @@
 import pandas as pd
 from bs4 import BeautifulSoup as bs
 import re
+import nltk
+nltk.download("stopwords")
+from nltk.corpus import stopwords
 
-#read in the data
+# Preprocess a given review.
+def from_review_to_words(review):
+    # Remove tags from one movie review
+    noTag = bs(review)
+
+    # Remove punctuation, change to lower case and split into words
+    letter_only = re.sub("[^a-zA-Z]", " ", noTag.get_text()).lower().split()
+
+    # Convert stopword dictionary to set.
+    stops = set(stopwords.words("english"))
+
+    # Remove stop words
+    removeStop = [w for w in letter_only if not w in stops]
+
+    # Convert back to sentence and return it
+    return (" ".join(removeStop))
+
+# Read in the data
 train = pd.read_csv("data\labeledTrainData.tsv", header=0, \
-                    delimiter = "\t", quoting=3)
+                        delimiter = "\t", quoting=3)
 
-#remove tags from one movie review
-noTag = bs(train["review"][0])
+# List for the clean reviews
+clean_reviews = []
 
-#remove punctuation
-letter_only = re.sub("[^a-zA-Z]", " ", noTag.get_text())
-print(train["review"][0])
-print letter_only
-
+#  Clean the reviews and append to clean_reviews
+for i, review in enumerate(train["review"]):
+    clean_reviews.append(from_review_to_words(review))
+    if(i % 1000 == 0):
+        print("We are currently at review %d" % i)
