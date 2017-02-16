a6f90807a2f666e6e82aaaa570ff3a01f547f063
Bas Vogelzang <bas.vogelzang95@gmail.com>
2017-02-16 17:37:33 +0100

    vectorizer to create data features and random forest classifier

--- readData.py
+++ readData.py
@@ -4,6 +4,8 @@ import re
 import nltk
 nltk.download("stopwords")
 from nltk.corpus import stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.ensemble import RandomForestClassifier
 
 # Preprocess a given review.
 def from_review_to_words(review):
@@ -23,7 +25,7 @@ def from_review_to_words(review):
     return (" ".join(removeStop))
 
 # Read in the data
-train = pd.read_csv("data\labeledTrainData.tsv", header=0, \
+train = pd.read_csv("data/trainingSample.tsv", header=0, \
                         delimiter = "\t", quoting=3)
 
 # List for the clean reviews
@@ -34,3 +36,18 @@ for i, review in enumerate(train["review"]):
     clean_reviews.append(from_review_to_words(review))
     if(i % 1000 == 0):
         print("We are currently at review %d" % i)
+
+# Create a vectorizer
+vectorizer = CountVectorizer(analyzer = "word", tokenizer = None, preprocessor = None, stop_words = None, max_features = 5000)
+
+# Fit_transform() 2 functions:
+# 1. Fits the model and learns vocabulary
+# 2. Transforms training data into feature vectors.
+data_features = vectorizer.fit_transform(clean_reviews)
+data_features = data_features.toarray()
+
+# Initializing random forest classifier and run it
+forest = RandomForestClassifier(n_estimators = 100)
+forest = forest.fit(data_features, train["sentiment"])
+
+#TODO: Test data
